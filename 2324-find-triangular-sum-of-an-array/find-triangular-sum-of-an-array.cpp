3cc46d15ea7e9e284cc7be23476b86866c313c03
PankajGiri054 <pankajgiri8273@gmail.com>
2026-07-12 15:30:15 +0530

    Time: 313 ms (28.69%) | Memory: 382.5 MB (25.43%) - LeetSync

---
+++ 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int triangularSum(vector<int>& nums) {
+        int n=nums.size();
+        while(n>1){
+        vector<int>temp;
+        for(int i=0;i<n-1;i++){
+int sum=(nums[i]+nums[i+1])%10;
+temp.push_back(sum);
+        }
+        nums=temp;
+       n=nums.size();
+        }
+        return nums[0];
+    }
+};
