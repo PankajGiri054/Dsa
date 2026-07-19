ca7cca87ced1113ff0731285d7386be29e3b5748
PankajGiri054 <pankajgiri8273@gmail.com>
2026-07-19 12:43:46 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (87.16%) - LeetSync

---
+++ 292-nim-game/nim-game.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool canWinNim(int n) {
+       // bool iwin=true;
+        if(n%4==0){
+return false;
+        }else{
+            return true;
+        }
+    }
+};
