465411996024f181eb842ff3def74a61f47e7781
PankajGiri054 <pankajgiri8273@gmail.com>
2026-07-19 12:46:51 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (87.16%) - LeetSync

--- 292-nim-game/nim-game.cpp
+++ 292-nim-game/nim-game.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool canWinNim(int n) {
-       // bool iwin=true;
+       // basically esme krna ye hai in first chance me ham 1,2,3 me se kitne bhi stone utha sakte hai agar stone count 4 ho to ham kitne bhi uthaye second move me friend jit jayega if  n==5 in first move i will pick 1 stone and friend choose 1,2,3, any one so in  next move i will definetly win so it is main logic of this game ,
         if(n%4==0){
 return false;
         }else{
