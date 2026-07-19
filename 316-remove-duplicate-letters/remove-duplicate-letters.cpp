2e989a2df8c57cf26a9c34b5bc8dd428d6ef03d8
PankajGiri054 <pankajgiri8273@gmail.com>
2026-07-19 12:15:56 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (51.99%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-        int n=s.length();
-        unordered_map<char,int>hash;
-        for(int i=0;i<n;i++){
+        int n = s.length();
+
+        unordered_map<char, int> hash;
+
+        for (int i = 0; i < n; i++) {
             hash[s[i]]++;
         }
-        string str="";
-        unordered_map<char,bool>visited;
-        for(int i=0;i<n;i++){
-hash[s[i]]--;
-if(visited[s[i]]){
-    continue;
-}
-while(str.length()>0&&str[str.length()-1]>s[i]&&hash[str[str.length()-1]]>0){
-    visited[str[str.length()-1]]=false;
-    str.erase(str.length()-1);
-}
-str+=s[i];
-visited[s[i]]=true;
+
+        string str = "";
+        unordered_map<char, bool> visited;
+
+        for (int i = 0; i < n; i++) {
+
+            hash[s[i]]--;
+
+            if (visited[s[i]]) {
+                continue;
+            }
+
+            while (str.length() > 0) {
+
+                int last = str.length() - 1;
+
+                if (str[last] > s[i] && hash[str[last]] > 0) {
+                    visited[str[last]] = false;
+                    str.erase(last);
+                } else {
+                    break;
+                }
+            }
+
+            str += s[i];
+            visited[s[i]] = true;
         }
+
         return str;
     }
 };
