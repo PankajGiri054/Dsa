3993ae9e64afed8e09f38fa826234e7a01fcd7c3
PankajGiri054 <pankajgiri8273@gmail.com>
2026-08-04 21:05:58 +0530

    Time: 0 ms (100.00%) | Memory: 32.3 MB (54.61%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& arr) {
-      int n=arr.size();
-      int high=arr[0];
-      int low=arr[0];
-      vector<int>ans;
-      for(int i=0;i<n;i++){
-        if(arr[i]>high){
-            high=arr[i];
+        int n = arr.size();
+        vector<int> ans;
+
+        sort(arr.begin(), arr.end());
+
+        int x = arr[0];
+
+        for (int i = 0; i < n; i++) {
+            while (x < arr[i]) {
+                ans.push_back(x);
+                x++;
+            }
+            x++;
         }
-      }
-        for(int i=0;i<n;i++){
-        if(arr[i]<low){
-            low=arr[i];
-        }
-      }
-      sort(arr.begin(), arr.end());
-      int x=low;
-      for(int i=0;i<n;i++){
-       while(x<arr[i]){
-        ans.push_back(x);
-        x++;
-       }
-       x++;
-        }
-      return ans;
+
+        return ans;
     }
 };
