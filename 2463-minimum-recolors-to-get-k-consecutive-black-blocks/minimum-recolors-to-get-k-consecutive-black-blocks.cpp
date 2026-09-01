83495dfae48a09bc8d2a61936f8f68fff29f70a8
PankajGiri054 <pankajgiri8273@gmail.com>
2026-09-01 22:20:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (92.69%) - LeetSync

---
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minimumRecolors(string blocks, int k) {
+        int white=0;
+        for(int i=0;i<k;i++){
+        if(blocks[i]=='W'){
+        white++;
+        }
+        }
+        int n=blocks.size();
+        int ans=white;
+        for(int i=k;i<n;i++){
+if(blocks[i-k]=='W'){
+    white--;
+}
+if(blocks[i]=='W'){
+    white++;
+}
+ans=min(ans,white);
+        }
+        return ans;
+    }
+};
